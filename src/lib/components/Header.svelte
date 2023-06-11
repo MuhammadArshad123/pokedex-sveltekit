5bbc4e67c1b1c92c59f7fcdfd3c4d490a39ffcf0
Muhammad Arshad <63872178+MuhammadArshad123@users.noreply.github.com>
2023-06-11 17:11:01 -0500

    Added gap to navbar

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <nav>
-    <ul class="list-none flex justify-center">
+    <ul class="list-none flex justify-center gap-3">
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
     </ul>
